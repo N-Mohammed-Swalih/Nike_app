a7429668b540195af8a504e72054deb73131266b
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-08 13:02:16 +0530

    commit

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -10,7 +10,7 @@ class ShoeTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(left: 25),
+      margin:const EdgeInsets.only(left: 25),
       width: 280,
       decoration: BoxDecoration(
         color: Colors.grey[200],
