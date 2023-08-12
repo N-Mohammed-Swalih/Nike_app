ac42a4bb95cadc4f3870710d9d66e12e43325662
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-12 19:46:28 +0530

    commit

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -9,19 +9,16 @@ class Cart extends ChangeNotifier {
         price: "456",
         imagePath: "assets/shoes/airmax1.jpg",
         description: "Live with airmax styish lifestyle"),
-
     Shoe(
         name: "Airmax Pulse",
         price: "236",
         imagePath: "assets/shoes/airmaxpulse.jpg",
         description: "Live with airmax plus styish lifestyle"),
-  
     Shoe(
         name: "Metcon 9",
         price: "327",
         imagePath: "assets/shoes/metcon9.jpg",
         description: "Live with metcon stylish lifestyle"),
-        
     Shoe(
         name: "Zoom Vomero 5",
         price: "777",
@@ -31,30 +28,27 @@ class Cart extends ChangeNotifier {
 
   //list of items in users cart
 
-  List<Shoe> userCart = [
-
-  ];
+  List<Shoe> userCart = [];
 
   //get list of the shoes for sale
 
-  List<Shoe> getShoeList(){
+  List<Shoe> getShoeList() {
     return shoeShop;
   }
+
   //get cart
-List<Shoe>getUserCart(){
-  return userCart;
-}
+  List<Shoe> getUserCart() {
+    return userCart;
+  }
 
   //add items to the cart
-  void addItemsToCart(Shoe shoe)
-  {
+  void addItemsToCart(Shoe shoe) {
     userCart.add(shoe);
     notifyListeners();
   }
 
   //remove item from the cart
-  void removeItemsfromCart(Shoe shoe)
-  {
+  void removeItemsfromCart(Shoe shoe) {
     userCart.remove(shoe);
     notifyListeners();
   }
